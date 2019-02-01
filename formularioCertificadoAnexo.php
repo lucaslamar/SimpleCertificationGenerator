d4098988047f8a009a718c146476e00dd3275fa8
leo23fla <leo23fla@gmail.com>
2019-02-01 10:06:13 -0200

    Reparo na forma da manipulação dos arquivos
    Modificado o código para manipulação dos arquivos independente do sistema operacional.

--- formularioCertificadoAnexo.php
+++ formularioCertificadoAnexo.php
@@ -1,16 +1,12 @@
-<!--
-Author: Colorlib
-Author URL: https://colorlib.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Gerador de Certificado</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- Custom Theme files -->
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <!-- //Custom Theme files -->
@@ -24,11 +20,11 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<h1>ASSINAR CERTIFICADO</h1>
 		<div class="main-agileinfo">
 			<div class="agileits-top">
-				<form enctype="multipart/form-data" action="imprimirCertificadoAnexo.php" method="post">
-					<input class="text email" type="text" name="nome-certificando" placeholder="Nome Certificando" required="">
-					<input class="text" type="text" name="tamanho-fonte" placeholder="Tamanho Fonte">
-					<input class="text w3lpass" type="text" name="eixo-x" placeholder="Eixo X">
-					<input class="text w3lpass" type="text" name="eixo-y" placeholder="Eixo Y">
+				<form enctype="multipart/form-data" action="" method="post">
+					<input class="text email" type="text" name="nome" required="">
+					<input class="text" type="text" name="fonte" placeholder="Tamanho Fonte">
+					<input class="text w3lpass" type="text" name="eixoX" placeholder="Eixo X">
+					<input class="text w3lpass" type="text" name="eixoY" placeholder="Eixo Y">
 					<div class="wthree-text">
 						<div class="clear"> </div>
 					</div>
@@ -36,27 +32,52 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<input type="submit" value="Gerar Certificado">
 					
 				</form>
-				
 			</div>
+			<?php
+				if (isset($_SESSION['imagem'])){
+					echo '<img src="data:image/jpeg;base64,' . $_SESSION['imagem'] . '" />';
+					unset($_SESSION['imagem']);
+					echo '<a href="'.$_SESSION['salvarPDF'].'" >Salvar PDF</a>';
+					echo '<a href="'.$_SESSION['salvarJPEG'].'" >Salvar JPEG</a>';
+				}
+				?>
 		</div>
-		<!-- copyright -->
-		<div class="colorlibcopy-agile">
-			<p>© 2018 Colorlib Signup Form. All rights reserved | Design by <a href="https://colorlib.com/" target="_blank">Colorlib</a></p>
-		</div>
-		<!-- //copyright -->
-		<ul class="colorlib-bubbles">
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
 	</div>
 	<!-- //main -->
 </body>
 </html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	unset($_SESSION['imagem']);
+	$uploaddir = 'certificado'. DIRECTORY_SEPARATOR .'';
+    $uploadfile = $uploaddir . basename($_FILES['anexo']['name']);
+	move_uploaded_file($_FILES['anexo']['tmp_name'], $uploadfile);
+$output = shell_exec( "gswin64c -sDEVICE=jpeg -r400 -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=certificado". DIRECTORY_SEPARATOR ."certificado.jpg ".$uploadfile."");
+$imge = imagecreatefromjpeg('certificado'. DIRECTORY_SEPARATOR .'certificado.jpg');
+$titleColor = imagecolorallocate($imge, 0, 0, 0);
+$gray = imagecolorallocate($imge, 120, 120, 120);
+
+$fonteBevan = __DIR__ . DIRECTORY_SEPARATOR . "fonts" . DIRECTORY_SEPARATOR . "Bevan-Regular.ttf";
+$fontePlayBall= __DIR__ . DIRECTORY_SEPARATOR . "fonts" . DIRECTORY_SEPARATOR . "Playball-Regular.ttf";
+
+// Quando for usar uma fonte própria segue a função e parametros necessário - Neste exemplo válido para fontes truetype (ttf)
+//imagettftext(image, size, angle, x, y, color, fontfile, text)
+//imagettftext($imge, 32, 0, 100, 150, $titleColor, $fonteBevan, $nomeCertificado);
+imagettftext($imge, 80, 0, 1400, 1400, $titleColor, $fonteBevan, $_POST['nome']);
+
+//imagestring($imge, 10, 0, 400, 380, $gray, $fontePlayBall, utf8_decode("Concluído em: ").date("d/m/Y"),$titleColor);
+//imagestring($imge, 3, 440, 410, utf8_decode("Concluido em : ") . date("d/m/Y"), $titleColor);
+ob_start (); 
+  imagejpeg($imge);
+  $image_data = ob_get_contents (); 
+ob_end_clean (); 
+$_SESSION['imagem'] = base64_encode ($image_data);
+imagejpeg($imge, "certificado".DIRECTORY_SEPARATOR."certificado.jpg",60); 
+$output = shell_exec( "magick convert certificado". DIRECTORY_SEPARATOR ."certificado.jpg certificado". DIRECTORY_SEPARATOR ."certificado.pdf");
+$_SESSION['salvarPDF'] = "certificado". DIRECTORY_SEPARATOR ."certificado.pdf";
+$_SESSION['salvarJPEG'] = "certificado". DIRECTORY_SEPARATOR ."certificado.jpg";
+header("Refresh:0");
+
+
+}
+?>
