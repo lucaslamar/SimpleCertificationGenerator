684201a001eb48c990168df52ce3753b0c812241
ArthurMaximino <maxarthur101@gmail.com>
2019-02-03 19:35:45 -0200

    I do like flowers during summer days

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # SimpleCertificationGenerator
-Um Simples Gerador de Certificado feito em PHP usando a biblioteca GD e GhostScript. Ao preencher o formulario e selecionado o certificado para upload é gerado um certificado que é mostrado na tela havendo a opção de gerar arquivos PDF ou JPEG.
+Um Simples Gerador de Certificado feito em PHP usando a biblioteca GD do PHP. Ao preencher o formulario é gerado um certificado que é mostrado na tela, além de se criar um arquivo .JPG do mesmo na pasta htdocs do xaamp.
 
-OBS: Até o momento somente arquivos .pdf funcionam.
+ATENÇÃO: Vale lembrar que os certificados gerados pelo "SimpleCertificationGenerator" NÃO SÃO VALIDOS é apenas uma base... E PARA ZOEIRAS EM GERAL, OBRIGADO!
 
-ATENÇÃO: Vale lembrar que os Certificados Ficticios gerados pelo "SimpleCertificationGenerator" NÃO SÃO VALIDOS é apenas uma base, Utilizada para entretenimento. 
+REQUISITOS PARA USO E COMO USAR
+TER XAAMP INSTALADADO NA MAQUINA;
+RODAR O APACHE;
+ACESSAR A SEGUINTE URL: http://localhost/Certificado/gerarCertificado.php 
---
+++ js/abreModal.js
@@ -0,0 +1,77 @@
+/*
+Hello, there. My name is Arthur and let me show you a few features I've made in this project! =)
+*/
+
+
+//First of all, let's design an animation for our modal:
+
+//Let's get the id of the elements we want to animate.
+var modal = document.getElementById("meuModal");
+var btn = document.getElementById("abreModal");
+
+
+
+//Now let's show the modal with an animation when the menu icon is clicked:
+btn.onclick = function() {
+    modal.style.webkitAnimationName = 'slide';
+    modal.style.webkitAnimationDuration = '.5s';
+    modal.style.display = "block";
+  }
+
+//Let's create an animation when the user closes the modal by clicking outside the window:
+window.onclick = function (event){
+  if (event.target == modal) {
+    modal.style.webkitAnimationName = 'slideBack';
+    modal.style.webkitAnimationDuration = '.5s';
+    setTimeout(function call(event) {
+      modal.style.display = "none";
+      }, 250);
+  }
+}
+
+
+//Now we have a few things to do when the page is finally loaded:
+  window.onload = function() { 
+// 1 - We're gonna set the class tag in the navigation drawer element which the user is currently on by adding the class "active" to that tag;
+    //Let's find the page which the user the user is right now and compare it to our list in the navigation bar
+    var all_links = document.getElementById("navInfoMenu").getElementsByTagName("a"),
+        i=0, len=all_links.length,
+        full_path = location.href.split('#')[0]; 
+
+    // Loop through each link.
+    for(; i<len; i++) {
+        if(all_links[i].href.split("#")[0] == full_path) {
+            all_links[i].className += " active";
+            
+        }
+    }
+    //Good! Now it's time to change the icon in the class active to the color we want, by changing the "fill" attribute in each one of the path tags in our svg file!
+    var getIcon = document.getElementsByClassName("infoItem active")[0];
+    getPathIcon = getIcon.getElementsByTagName("path");
+    //vamosVer.style.fill = "#EA2027";
+    for (n=0;n<=getPathIcon.length;n++)
+    {
+    getPathIcon[n].style.fill = "#EA2027"
+    }
+
+}
+
+//I'm using a library called Typed.js, for an awesome effect on the form title!
+//This is the code used to print the string "Requerimentos" with the wished effect.
+var typed = new Typed('#requerimentos', {
+    strings: ["Requerimentos"],
+    typeSpeed: 40,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false
+  });
+
+
+$('#inputCert').on('change',function(){
+                //get the file name
+                var fileName = document.getElementById("inputCert").files[0].name;
+                //replace the "Choose a file" label
+                $(this).next('.custom-file-label').html(fileName);
+            })
+
+
