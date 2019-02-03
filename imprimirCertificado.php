0853ee02dd1cb035658b57e1e8249e455a97c72c
lucas lamar <lucass.lamar@gmail.com>
2019-02-03 17:59:45 -0200

    preparação integração front end

--- imprimirCertificado.php
+++ imprimirCertificado.php
@@ -1,39 +1,57 @@
 <?php
+session_start();
+    // verificando se o diretorio existe e criando se nescessario
+
+    $diretorio = "certificado";
+    if (!file_exists($diretorio)){
+        mkdir("$diretorio", 0700);
+        }
+// recebendo parametros formulario
+    $nomeCertificando = $_POST['nome'];
+    $tamanhoFonte = (int)$_POST['fonte'];
+    $eixoX = (int)$_POST['eixoX'];
+    $eixoY = (int)$_POST['eixoY'];
     
-    $nomeCertificando = $_POST['nome-certificando'];
-    $nomeCertificado = $_POST['nome-certificado'];
-    $tamanhoFonte = (int)$_POST['tamanho-fonte'];
-    $eixoX = (int)$_POST['eixo-x'];
-    $eixoY = (int)$_POST['eixo-y'];
+    // upload do certificado 
+
+    unset($_SESSION['imagem']);
+    $uploaddir = 'certificado'. DIRECTORY_SEPARATOR .'';
+    $uploadfile = $uploaddir . basename($_FILES['anexo']['name']);
+    move_uploaded_file($_FILES['anexo']['tmp_name'], $uploadfile);
 
       // tratando parametros passados vazios
       if (empty($tamanhoFonte)) $tamanhoFonte ?: $tamanhoFonte = 22;
       if (empty($eixoX)) $eixoX ?: $eixoX = 480;
       if (empty($eixoY)) $eixoY ?: $eixoY = 320;
 
-    // ESPECICAÇÕES IMAGEM
-    // Exemplo de como gerar imagem a partir de um modelo base
-    $imge = imagecreatefromjpeg("certificado-base.jpg");
+
+
+    $output = shell_exec( "gswin64c -sDEVICE=jpeg -r400 -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=certificado". DIRECTORY_SEPARATOR ."certificado.jpg ".$uploadfile."");
+    $imge = imagecreatefromjpeg('certificado'. DIRECTORY_SEPARATOR .'certificado.jpg');
     $titleColor = imagecolorallocate($imge, 0, 0, 0);
     $gray = imagecolorallocate($imge, 120, 120, 120);
 
+    // fontes
     $fonteBevan = __DIR__ . DIRECTORY_SEPARATOR . "fonts" . DIRECTORY_SEPARATOR . "Bevan-Regular.ttf";
     $fontePlayBall= __DIR__ . DIRECTORY_SEPARATOR . "fonts" . DIRECTORY_SEPARATOR . "Playball-Regular.ttf";
-
-    // Quando for usar uma fonte própria segue a função e parametros necessário - Neste exemplo válido para fontes truetype (ttf)
+                
+    //Quando for usar uma fonte própria segue a função e parametros necessário - Neste exemplo válido para fontes truetype (ttf)
     //imagettftext(image, size, angle, x, y, color, fontfile, text)
-    imagettftext($imge, 32, 0, 100, 150, $titleColor, $fonteBevan, $nomeCertificado);
-    imagettftext($imge, $tamanhoFonte, 0, $eixoX, $eixoY, $titleColor, $fontePlayBall, $nomeCertificando);
-
+    //imagettftext($imge, 32, 0, 100, 150, $titleColor, $fonteBevan, $nomeCertificado);
+    
+    imagettftext($imge, $tamanhoFonte, 0, $eixoX, $eixoY, $titleColor, $fonteBevan, $nomeCertificando);
+    // imprime data atual se preciso        
     //imagestring($imge, 10, 0, 400, 380, $gray, $fontePlayBall, utf8_decode("Concluído em: ").date("d/m/Y"),$titleColor);
-    imagestring($imge, 3, 440, 410, utf8_decode("Concluido em : ") . date("d/m/Y"), $titleColor);
-
-
-    // GERANDO A IMAGEM
-    header("Content-type: image/jpeg");
-    imagejpeg($imge); ; // Mostra na tela certificado
-    $nomeSemEspacos = str_replace(' ', '', $nomeCertificando); 
-    imagejpeg($imge, "certificado-".mb_strtolower($nomeSemEspacos,"utf-8").".jpg",60); // salva arquivo .JPG na pasta htdocs
-    imagedestroy($imge);
-    echo unlink($uploadfile); // Exclui imagem usada como base
+    //imagestring($imge, 3, 440, 410, utf8_decode("Concluido em : ") . date("d/m/Y"), $titleColor);
+    
+    ob_start (); 
+    imagejpeg($imge);
+    $image_data = ob_get_contents (); 
+    ob_end_clean (); 
+    $_SESSION['imagem'] = base64_encode ($image_data);
+    imagejpeg($imge, "certificado".DIRECTORY_SEPARATOR."certificado.jpg",60); 
+    $output = shell_exec( "magick convert certificado". DIRECTORY_SEPARATOR ."certificado.jpg certificado". DIRECTORY_SEPARATOR ."certificado.pdf");
+    $_SESSION['salvarPDF'] = "certificado". DIRECTORY_SEPARATOR ."certificado.pdf";
+    $_SESSION['salvarJPEG'] = "certificado". DIRECTORY_SEPARATOR ."certificado.jpg";
+    header("location:formularioCertificado.php");                
 ?>
