f3886b30cf60ee1020cef87dacf390f8fd13f492
Leonardo <leo_fla_bsb@hotmail.com>
2019-02-18 22:17:11 -0300

    Alterado execução saida do sistema

--- php/imprimirCertificado.php
+++ php/imprimirCertificado.php
@@ -2,6 +2,13 @@
 session_start();
 require_once('funcoes.php');
 $funcao = new Funcoes(); 
+if(isset($_POST['action']) && !empty($_POST['action'])) {
+    session_unset();
+    $diretorioCertificado = __DIR__.DIRECTORY_SEPARATOR."certificado". DIRECTORY_SEPARATOR;
+    $diretorioFonte = __DIR__.DIRECTORY_SEPARATOR."fontes". DIRECTORY_SEPARATOR;
+    array_map('unlink', glob($diretorioCertificado .'*' .'*'));
+    array_map('unlink', glob($diretorioFonte .'*' .'*'));
+}
 //unset ($_SESSION['gerarIRT']);
 $verificaimagick = $funcao -> verificaimagick();
 $_SESSION['verificaimagick'] = $verificaimagick;
@@ -26,7 +33,7 @@ if (isset ($_SESSION['gerarIRT']) && $_SESSION['gerarIRT'] == true ){
     $_SESSION['fonteTamanho'] = $fonte;
     $teste = $funcao -> gerarIRT($verificaGhostscript);
 }
-else{	
+else{   
     $verificaPastas = $funcao -> verificaPastas();
     if ($_FILES['fonteAnexo']['tmp_name'] != ""){
         $verificaFonte = $funcao -> verificaFonte($_FILES['fonteAnexo']['tmp_name']);
@@ -37,7 +44,7 @@ else{
         $verificaFonte = dirname(__DIR__, 1). DIRECTORY_SEPARATOR . 'fontes'. DIRECTORY_SEPARATOR."DancingScript-Regular.ttf"; 
         $_SESSION['fonte'] = $verificaFonte;
     }
-	// recebendo parametros formulario
+    // recebendo parametros formulario
     $nomeCertificando = $_POST['nome-certificando'];
     $_SESSION['nome'] = $nomeCertificando;
     $tamanhoFonte = (int)$_POST['tamanho-fonte'];
@@ -50,12 +57,5 @@ else{
     $_SESSION['pdf'] = $verificaAnexo;
     $escreveJPEG = $funcao -> escreveJPEG($dados);
     $geraPDF = $funcao -> geraPDF($verificaimagick);
-}
-if(isset($_POST['action']) && !empty($_POST['action'])) {
-    session_unset();
-    $diretorioCertificado = __DIR__.DIRECTORY_SEPARATOR."certificado". DIRECTORY_SEPARATOR;
-    $diretorioFonte = __DIR__.DIRECTORY_SEPARATOR."fontes". DIRECTORY_SEPARATOR;
-    array_map('unlink', glob($diretorioCertificado .'*' .'*'));
-    array_map('unlink', glob($diretorioFonte .'*' .'*'));
 }
 ?>
